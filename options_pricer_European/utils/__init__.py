ac83f889852c1e7843ba50dedd3fe7f4224104f6
Aayush <143271756+sunno-bhaato@users.noreply.github.com>
2025-08-10 15:57:43 +0530

    corrected line 2 (class name)

--- options_pricer_European/utils/__init__.py
+++ options_pricer_European/utils/__init__.py
@@ -1,5 +1,5 @@
 from .Greeks import delta, gamma, theta, vega
-from .Visualisation_Tools_Black_Scholes import BS_Options_Visualizer
+from .Visualisation_Tools_Black_Scholes import BSOptionsVisualizer
 from .Visualisation_Tools_Monte_Carlo import MC_Visualiser
 from .Strategies import Bull_Call_Spread, Bull_Put_Spread, Bear_Call_Spread, Bear_Put_Spread, Collar, Straddle, Strangle
 from .IV import IV_NewRaph, IV_Brent, IV_Binomial_Bisection
