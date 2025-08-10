8e4fe3ec0120ee4dc34339eb6092058b3b1ba9a8
Aayush <143271756+sunno-bhaato@users.noreply.github.com>
2025-08-10 16:00:59 +0530

    corrected Strategies to strategies in line 4

--- options_pricer_European/utils/__init__.py
+++ options_pricer_European/utils/__init__.py
@@ -1,7 +1,7 @@
 from .Greeks import delta, gamma, theta, vega
 from .Visualisation_Tools_Black_Scholes import BSOptionsVisualizer
 from .Visualisation_Tools_Monte_Carlo import MC_Visualiser
-from .Strategies import Bull_Call_Spread, Bull_Put_Spread, Bear_Call_Spread, Bear_Put_Spread, Collar, Straddle, Strangle
+from .strategies import Bull_Call_Spread, Bull_Put_Spread, Bear_Call_Spread, Bear_Put_Spread, Collar, Straddle, Strangle
 from .IV import IV_NewRaph, IV_Brent, IV_Binomial_Bisection
 
 __all__ = ['delta', 'gamma', 'theta', 'vega', 'Bull_Call_Spread', 'Bull_Put_Spread', 'Bear_Call_Spread', 'Bear_Put_Spread', 
