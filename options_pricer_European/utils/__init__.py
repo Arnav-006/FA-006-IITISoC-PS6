f1edef218bba1db94a2e0b35c9db98e2111840a8
Aayush <143271756+sunno-bhaato@users.noreply.github.com>
2025-08-10 16:05:28 +0530

    minor correction

--- options_pricer_European/utils/__init__.py
+++ options_pricer_European/utils/__init__.py
@@ -5,5 +5,5 @@ from .strategies import Bull_Call_Spread, Bull_Put_Spread, Bear_Call_Spread, Bea
 from .IV import IV_NewRaph, IV_Brent, IV_Binomial_Bisection
 
 __all__ = ['delta', 'gamma', 'theta', 'vega', 'Bull_Call_Spread', 'Bull_Put_Spread', 'Bear_Call_Spread', 'Bear_Put_Spread', 
-           'Collar', 'Straddle', 'Strangle', 'BS_Options_Visualizer', 'MC_Visualiser', 'IV_NewRaph', 'IV_Brent', 
+           'Collar', 'Straddle', 'Strangle', 'BSOptionsVisualizer', 'MC_Visualiser', 'IV_NewRaph', 'IV_Brent', 
            'IV_Binomial_Bisection']
